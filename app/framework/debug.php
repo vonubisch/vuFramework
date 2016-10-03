19669651c400848068043c3c8e1b76c2c638beff
Bjorn S. von Ubisch <bs@vonubisch.com>
2016-10-03 12:22:32 +0200

    Third commit

--- app/framework/debug.php
+++ app/framework/debug.php
@@ -1,14 +1,13 @@
 <?php
 
 /**
- * Accepts all requests, load and start application
+ * Debug
  *
  * @package    vuFramework
  * @version    v8.0
  * @author     Bjorn Stefan von Ubisch <bs@vonubisch.com>
  * @copyright  2016 vonUbisch.com
  */
-require_once 'app/framework/application.php';
-
-$app = new Application();
-$app->start();
+class Debug {
+    
+}
